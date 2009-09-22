1f2709970483df6125384761f4430666eb1e4970
Martin Smith <martin.smith@blueskyts.co.uk>
2009-09-22 13:54:17 +0100

    Added priority in a MoSCoW stylee

--- grails-app/domain/Item.groovy
+++ grails-app/domain/Item.groovy
@@ -7,8 +7,12 @@ class Item implements Commentable {
     boolean fixed
     boolean passed
 
+    Priority priority
+
     static belongsTo = Group
 
     static constraints = {
     }
 }
+
+enum Priority { MUST_HAVE, SHOULD_HAVE, COULD_HAVE, WONT_HAVE }
